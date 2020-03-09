d0551505cfd47c78afe6e9d344f167979a21932f
hoz1982 <hoz1982@users.noreply.github.com>
2020-03-09 13:25:44 +0100

    09/03/2020 Update

--- views/wp_partita_iva-settings/page-settings.php
+++ views/wp_partita_iva-settings/page-settings.php
@@ -1,6 +1,6 @@
 <div class="wrap">
 	<div id="icon-options-general" class="icon32"><br /></div>
-	<h1><?php esc_html_e( wp_partita_iva_NAME ); ?> Settings</h1>
+	<h1><?php esc_html_e( wp_partita_iva_NAME ); ?> Impostazioni</h1>
 
 	<form method="post" action="options.php">
 		<?php settings_fields( 'wp_partita_iva_settings' ); ?>
