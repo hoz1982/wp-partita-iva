1c350cec703b56d43ad9b02680d564dd60513a6f
hoz1982 <hoz1982@users.noreply.github.com>
2020-08-20 11:12:03 +0200

    Aggiunta checkbox "Necessaria fattura elettronica si/no"

--- classes/wordpress_partita_iva.php
+++ classes/wordpress_partita_iva.php
@@ -300,6 +300,7 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
             $required_nin = $settings['advanced']['field-obb-nin'];
             $required_pec = $settings['advanced']['field-obb-pec'];
 
+
             if ($abilitazione_cf == 1) {
                 $fields['billing']['billing_cf'] = array(
                     'label' => __('Codice Fiscale', 'woocommerce'),
@@ -307,6 +308,7 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
                     'required' => $required_cf,
                     'clear' => false,
                     'type' => 'text',
+                    'priority' => 32,
                     'class' => array('form-row-wide')
                 );
             }
@@ -317,6 +319,7 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
                     'required' => $required_pi,
                     'clear' => false,
                     'type' => 'text',
+                    'priority' => 33,
                     'class' => array('form-row-wide')
                 );
             }
@@ -327,6 +330,7 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
                     'required' => $required_nin,
                     'clear' => false,
                     'type' => 'text',
+                    'priority' => 34,
                     'class' => array('form-row-wide')
                 );
             }
@@ -337,9 +341,19 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
                     'required' => $required_pec,
                     'clear' => false,
                     'type' => 'text',
+                    'priority' => 35,
                     'class' => array('form-row-wide')
                 );
             }
+            $fields['billing']['billing_fatt'] = array(
+                'label' => __('HO BISOGNO DELLA FATTURA ELETTRONICA', 'woocommerce'),
+                'placeholder' => '',
+                'required' => 0,
+                'clear' => false,
+                'type' => 'checkbox',
+                'priority' => 31,
+                'class' => array('form-row-wide')
+            );
             return $fields;
         }
     add_filter('woocommerce_checkout_fields', 'wp_partita_iva_set_customer_billing_fields_in_checkout');
@@ -412,6 +426,8 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
                 if ($key == 'billing_pec')
                     return get_user_meta($current_user->ID, 'billing_pec', true);
             }
+            if ($key == 'billing_fatt')
+                return get_user_meta($current_user->ID, 'billing_fatt', true);
         }
     add_filter('woocommerce_checkout_get_value', 'wp_partita_iva_populate_customer_billing_fields_in_checkout', 10, 2 );
 
@@ -442,6 +458,9 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
                 if (!empty($_POST['billing_pec']))
                     update_user_meta($user_id, 'billing_pec', sanitize_text_field($_POST['billing_pec']));
             }
+            if (!empty($_POST['billing_fatt']))
+                $dato_fatt = 'Si';
+            update_user_meta($user_id, 'billing_fatt', sanitize_text_field($dato_fatt));
         }
     add_action( 'woocommerce_customer_save_address', 'wp_partita_iva_store_customer_billing_fields_in_address' );
 
@@ -472,6 +491,10 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
                 if (!empty($_POST['billing_pec']))
                     update_user_meta($user_id, 'billing_pec', sanitize_text_field($_POST['billing_pec']));
             }
+            if (!empty($_POST['billing_fatt']))
+                $dato_fatt = 'Si';
+            update_user_meta($user_id, 'billing_fatt', sanitize_text_field($dato_fatt));
+            //  update_user_meta($user_id, 'billing_fatt', sanitize_text_field($_POST['billing_fatt']));
         }
 
     add_action( 'woocommerce_checkout_update_user_meta', 'wp_partita_iva_store_customer_billing_fields_in_checkout' );
@@ -486,15 +509,75 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
             $billing_cf = trim($_POST['billing_cf']);
             $billing_nin = trim( $_POST['billing_nin'] );
             $billing_pec = trim( $_POST['billing_pec'] );
+            $billing_pec = trim($_POST['billing_fatt']);
 
-            if (empty($billing_vat) && empty($billing_cf))
-                wc_add_notice( __( 'You must insert a Codice Fiscale or Vat Code.' ), 'error' );
-            if (empty($billing_nin) && empty($billing_pec))
-                wc_add_notice( __( 'Devi inserire un Codice Cliente o una casella di Posta Elettronica Certificata.' ), 'error' );
+            // if (empty($billing_vat) && empty($billing_cf))
+            //   wc_add_notice( __( 'You must insert a Codice Fiscale or Vat Code.' ), 'error' );
+            //if (empty($billing_nin) && empty($billing_pec))
+            //  wc_add_notice( __( 'Devi inserire un Codice Cliente o una casella di Posta Elettronica Certificata.' ), 'error' );
         }
-    add_action('woocommerce_checkout_process', 'wp_partita_iva_validate_customer_billing_fields_in_checkout');
+
+    // add_action('woocommerce_checkout_process', 'wp_partita_iva_validate_customer_billing_fields_in_checkout');
+
+    /*    Imposto l'ordine di apparizione dei campi del form
+                */
 
 
+    function wp_partita_iva_woocommerce_default_address_fields($fields)
+    {
+
+        // default priorities:
+        // 'first_name' - 10
+        // 'last_name' - 20
+        // 'company' - 30
+        // 'country' - 40
+        // 'address_1' - 50
+        // 'address_2' - 60
+        // 'city' - 70
+        // 'state' - 80
+        // 'postcode' - 90
+
+        // e.g. move 'company' above 'first_name':
+        // just assign priority less than 10
+        // $fields['field-pi']['priority'] = 0;
+        //    $fields['state']['type'] = 'text';
+        //  unset($fields['state']);
+        // unset($fields['billing_state']);
+        // unset($fields['shipping_state']);
+
+        //  echo '<pre>';
+        //  print_r($fields);
+        //  echo '</pre>';
+
+        // return $fields;
+    }
+
+    /*ORIONBOTTLE - RIMUOVO BILLING STATE E INSERISCO NUOVO CAMPO PROVINCIA
+    */
+
+    add_filter('woocommerce_checkout_fields', 'wp_partita_iva_partial_unsetting_checkout_fields');
+    function wp_partita_iva_partial_unsetting_checkout_fields($fields)
+    {
+        unset($fields['billing']['billing_state']);
+        unset($fields['shipping']['shipping_state']);
+
+        return $fields;
+    }
+
+    // Reinserting custom billing state and post code checkout fields
+    add_filter('woocommerce_default_address_fields', 'wp_partita_iva_override_default_address_fields');
+    function wp_partita_iva_override_default_address_fields($address_fields)
+    {
+
+        // @ for state
+        $address_fields['billing_state']['type'] = 'text';
+        $address_fields['billing_state']['class'] = array('form-row-wide');
+        $address_fields['billing_state']['required'] = false;
+        $address_fields['billing_state']['label'] = __('State', 'woocommerce');
+        $address_fields['billing_state']['placeholder'] = __('State', 'woocommerce');
+
+        return $address_fields;
+    }
         /*    Aggiungo campi custom nel riepilogo ordine
         */
         function wp_partita_iva_add_customer_billing_fields_in_admin_order_meta( $order )
@@ -510,34 +593,38 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
                 $billing_vat = get_user_meta($order->get_user_id(), 'billing_vat', true);
                 $order_billing_vat = get_post_meta($orderid, '_billing_vat', true);
                 $client_vat_label = '<p><strong>' . __('Partita IVA', 'woocommerce') . ' </strong><br>';
-                $client_vat_label .= ($billing_vat) ? '(From Billing Address)' . $billing_vat : ($order_billing_vat) ? '(From Order Note)' . $order_billing_vat : 'N/D';
+                $client_vat_label .= ($billing_vat) ? '(From Billing Address)' . $billing_vat : ($order_billing_vat) ? '(From Order Note)' . $order_billing_vat : '0';
                 echo $client_vat_label . '</p>';
             }
             if ($abilitazione_cf == 1) {
                 $billing_cf = get_user_meta($order->get_user_id(), 'billing_cf', true);
                 $order_billing_cf = get_post_meta($orderid, '_billing_cf', true);
                 $billing_cf_label = '<p><strong>' . __('Codice Fiscale', 'woocommerce') . '</strong><br>';
-                $billing_cf_label .= ($billing_cf) ? $billing_cf : ($order_billing_cf) ? $order_billing_cf : 'N/D';
+                $billing_cf_label .= ($billing_cf) ? $billing_cf : ($order_billing_cf) ? $order_billing_cf : 'no';
                 echo $billing_cf_label . '</p>';
             }
             if ($abilitazione_nin == 1) {
                 $billing_nin = get_user_meta($order->get_user_id(), 'billing_nin', true);
                 $order_billing_nin = get_post_meta($orderid, '_billing_nin', true);
                 $billing_nin_label = '<p><strong>' . __('Codice Univoco', 'woocommerce') . '</strong><br>';
-                $billing_nin_label .= ($billing_nin) ? $billing_nin : ($order_billing_nin) ? $order_billing_nin : 'N/D';
+                $billing_nin_label .= ($billing_nin) ? $billing_nin : ($order_billing_nin) ? $order_billing_nin : 'no';
                 echo $billing_nin_label . '</p>';
             }
             if ($abilitazione_pec == 1) {
                 $billing_pec = get_user_meta($order->get_user_id(), 'billing_pec', true);
                 $order_billing_pec = get_post_meta($orderid, '_billing_pec', true);
                 $billing_pec_label = '<p><strong>' . __('Indirizzo PEC', 'woocommerce') . '</strong><br>';
-                $billing_pec_label .= ($billing_pec) ? $billing_pec : ($order_billing_pec) ? $order_billing_pec : 'N/D';
+                $billing_pec_label .= ($billing_pec) ? $billing_pec : ($order_billing_pec) ? $order_billing_pec : 'no';
                 echo $billing_pec_label . '</p>';
             }
+            $billing_fatt = get_user_meta($order->get_user_id(), 'billing_fatt', true);
+            $order_billing_fatt = get_post_meta($orderid, '_billing_fatt', true);
+            $billing_fatt_label = '<p><strong>' . __('Necessaria Fattura', 'woocommerce') . '</strong><br>';
+            $billing_fatt_label .= ($billing_fatt) ? $billing_fatt : ($order_billing_fatt) ? $order_billing_fatt : 'no';
+            echo $billing_fatt_label . '</p>';
     }
     add_action( 'woocommerce_admin_order_data_after_billing_address', 'wp_partita_iva_add_customer_billing_fields_in_admin_order_meta', 10, 1 );
-
-
+//    add_filter( 'woocommerce_default_address_fields', 'wp_partita_iva_woocommerce_default_address_fields' );
         /*    Salvataggio campi fattura elettronica se utente non chiede di registrarsi (ospite)
         */
         function wp_partita_iva_before_checkout_create_order( $order )
@@ -548,6 +635,129 @@ if ( ! class_exists( 'WordPress_Partita_IVA' ) ) {
             update_post_meta($order->id, '_billing_nin', sanitize_text_field($billing_nin));
             update_post_meta( $order->id, '_billing_pec', sanitize_text_field( $billing_pec ) );
         }
+
     add_action('woocommerce_checkout_update_order_meta', 'wp_partita_iva_before_checkout_create_order');
+
+    add_filter('wp_mail_content_type', 'wpdocs_set_html_mail_content_type');
+    // Utility function sending the email notification
+    function send_seller_email($order)
+    {
+        $data = array();
+        $orderid = $order->get_id();
+        // Get and Loop Over Order Items
+        // Loop through each order item
+        foreach ($order->get_items() as $item_id => $item) {
+            if (get_post_meta($order->get_id(), '_sent_to_seller_' . $item_id, true))
+                continue; // Go to next loop iteration
+
+            $product_id = $item->get_product_id();
+            // Untested part
+            //  $soldby =  wp_get_object_terms( $product_id, 'soldby', false );
+            //  if( empty($soldby) ) continue; // Go to next loop iteration
+            //  $seller_id = $soldby[0]->name;// $seller = get_user_by( 'ID', $seller_id );
+            // $seller->user_email = 'maildialex@gmail.com';
+            // $seller_email = 'info@blacklotus.eu';
+            $seller_email = 'info@orionbottle.com';
+
+            // Set the data in an array (avoiding seller email repetitions)
+            $data[$seller_email][] = array(
+                'excerpt' => get_the_excerpt($product_id),
+                'title' => get_the_title($product_id),
+                'link' => get_permalink($product_id),
+            );
+            // Update order to avoid notification repetitions
+            update_post_meta($order->get_id(), '_sent_to_seller_' . $item_id, true);
+        }
+
+        if (count($data) == 0) return;
+        $nome = get_post_meta($orderid, '_billing_first_name', true);
+        $cognome = get_post_meta($orderid, '_billing_last_name', true);
+        $company = get_post_meta($orderid, '_billing_company', true);
+        $email = get_post_meta($orderid, '_billing_email', true);
+        $tel = get_post_meta($orderid, '_billing_phone', true);
+        $indirizzo1 = get_post_meta($orderid, '_billing_address_1', true);
+        $indirizzo2 = get_post_meta($orderid, '_billing_address_2', true);
+        $city = get_post_meta($orderid, '_billing_city', true);
+        $state = get_post_meta($orderid, '_billing_state', true);
+        $cap = get_post_meta($orderid, '_billing_postcode', true);
+        $vat = get_post_meta($orderid, '_billing_vat', true);
+        $cf = get_post_meta($orderid, '_billing_cf', true);
+        $nin = get_post_meta($orderid, '_billing_nin', true);
+        $pec = get_post_meta($orderid, '_billing_pec', true);
+        $richiesta = get_post_meta($orderid, '_billing_fatt', true);
+
+        $subtotale = $order->get_subtotal();
+        $tasse = $order->get_total_tax();
+        $totale = $order->get_total();
+
+        // Loop through custom data array to send mails to sellers
+        foreach ($data as $email_key => $values) {
+            $to = $email_key;
+            $subject_arr = array();
+            $message = '';
+
+            foreach ($values as $value) {
+                $subject_arr[] = $value['title'];
+                $message .= 'Buongiorno,<br>';
+                $message .= 'è stato richiesto di emettere una nuova fattura dal negozio online Orionbottle. Di seguito i dettagli per la fattura elettronica:<br>';
+                $message .= 'ID ORDINE: ' . $orderid . ' <br>';
+                $message .= 'NOME: ' . $nome . ' <br>';
+                $message .= 'COGNOME:' . $cognome . ' <br>';
+                $message .= 'AZIENDA: ' . $company . ' <br>';
+                $message .= 'EMAIL: ' . $email . ' <br>';
+                $message .= 'TELEFONO: ' . $tel . ' <br>';
+                $message .= 'INDIRIZZO: ' . $indirizzo1 . ' ' . $indirizzo2 . ' ' . $city . ' ' . $state . ' ' . $cap . ' ' . ' <br>';
+                $message .= 'PARTITA IVA: ' . $vat . ' <br>';
+                $message .= 'CODICE FISCALE: ' . $cf . ' <br>';
+                $message .= 'CODICE CLIENTE PER FATTURAZIONE ELETTRONICA: ' . $nin . ' <br>';
+                $message .= 'INDIRIZZO PEC PER FATTURAZIONE ELETTRONICA: ' . $pec . ' <br>';
+                $message .= 'IMPONIBILE: ' . $subtotale . ' <br>';
+                $message .= 'IVA: ' . $tasse . ' <br>';
+                $message .= 'TOTALE: ' . $totale . ' <br>';
+                $message .= 'RICHIESTA FATTURA: ' . $richiesta . ' <br>';
+
+            }
+            $subject = 'Richiesta di invio fattura dal negozio online Orionbottle: ';
+
+            // Send email to seller
+            wp_mail($to, $subject, $message);
+            //    remove_filter( 'wp_mail_content_type', 'wpdocs_set_html_mail_content_type' );
+
+        }
+        //   exit();
+    }
+
+    add_action('woocommerce_new_order', 'new_order_seller_notification', 10, 1);
+    function new_order_seller_notification($order_id)
+    {
+        $order = wc_get_order($order_id);
+        $richiesta = get_post_meta($order_id, '_billing_fatt', true);
+
+        if (!($order->has_status('completed')))
+            return; // Exit
+        if ($richiesta == 'Si') {
+            send_seller_email($order);
+        }
+    }
+
+    add_action('woocommerce_order_status_changed', 'order_status_seller_notification', 20, 4);
+    function order_status_seller_notification($order_id, $status_from, $status_to, $order)
+    {
+        $richiesta = get_post_meta($order_id, '_billing_fatt', true);
+
+        if (!($status_to == 'completed'))
+            return; // Exit
+
+        if ($richiesta == 'Si') {
+            send_seller_email($order);
+        }
+
+    }
+
+    function wpdocs_set_html_mail_content_type()
+    {
+        return 'text/html';
+    }
+
 
 }
